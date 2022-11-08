9897104a9e311848ca891472a36545d8bd693cab
Gena-Tin <111846926+Gena-Tin@users.noreply.github.com>
2022-11-08 19:33:54 +0000

    Deploying to gh-pages from @ Gena-Tin/goit-js-hw-09@bbf161dc001fd09291abe4b47e406e262d9b20bb 🚀

---
+++ 01-color-switcher.1cfd4704.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("body"),t=document.querySelector("[data-start]"),a=document.querySelector("[data-stop]");a.disabled=!0;var d=null;t.addEventListener("click",(function(){t.disabled=!0,a.disabled=!1,d=setInterval((function(){e.style.background="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),a.addEventListener("click",(function(){clearInterval(d),t.disabled=!1,a.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.1cfd4704.js.map
--- 01-color-switcher.ae105b18.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("body"),e=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]");n.disabled=!0;var a=null;e.addEventListener("click",(function(){e.disabled=!0,n.disabled=!1,a=setInterval((function(){t.style.background="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),n.addEventListener("click",(function(){clearInterval(a),e.disabled=!1,n.disabled=!0}))}();
-//# sourceMappingURL=01-color-switcher.ae105b18.js.map
