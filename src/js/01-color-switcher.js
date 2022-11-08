55fd8e0111d9b7e6ede730510547258c3d8f8e42
GenaTin <111846926+Gena-Tin@users.noreply.github.com>
2022-11-08 19:07:37 +0200

    01 color switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
 
+const body = document.querySelector("body");
+const startBtn = document.querySelector("button[data-start]");
+const stopBtn = document.querySelector("button[data-stop]");
+
+stopBtn.disabled = true;
+
+let bgSwitcherId = null;
+
+startBtn.addEventListener("click", () => {
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+
+    bgSwitcherId = setInterval(() => {
+        body.style.background = getRandomHexColor()
+    }
+        , 1000);
+})
+
+stopBtn.addEventListener("click", () =>{
+    clearInterval(bgSwitcherId);
+
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+})
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
